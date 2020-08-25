78cb2af240330f2a8bdcff7ee2d48e125216d9e8
Bagaskaramadhan <bagaskaramadhan@gmail.com>
2020-08-26 01:27:40 +0700

    90

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+const button = document.querySelector('.hideimgSidemenu')
+const maincart = document.querySelector('.main-car')
+button.addEventListener('click',function(){
+    maincart.classList.toggle('slider')
+})
+// {/* <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> */}
